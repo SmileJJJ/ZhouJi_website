e9859d1f56c4f8866153df7f1c33129363a8dcb8
nan xi <2014896347@qq.com>
2018-04-29 15:13:18 +0800

    成员系统制作中~

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,120 @@
 
 @section('content')
 
+{{--<div class="container">--}}
+    {{--<div class="row justify-content-center">--}}
+        {{--<div class="col-md-8">--}}
+            {{--<div class="card">--}}
+                {{--<div class="card-header">提示</div>--}}
+
+                {{--<div class="card-body">--}}
+                    {{--@if (session('status'))--}}
+                        {{--<div class="alert alert-success">--}}
+                            {{--{{ session('status') }}--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
+
+                    {{--你已成功登陆！--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+
+<!-- 头部 -->
+<div class="jumbotron">
+    <div class="container">
+        <h2>宁波洲际信息科技有限公司</h2>
+        <p> 成员信息       仓库系统</p>
+    </div>
+</div>
+
+<!-- 中间内容区局 -->
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">提示</div>
+    <div class="row">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    你已成功登陆！
-                </div>
+        <!-- 左侧菜单区域   -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="#" class="list-group-item">成员列表</a>
+                <a href="#" class="list-group-item">新增成员</a>
             </div>
         </div>
+
+        <!-- 右侧内容区域 -->
+        <div class="col-md-9">
+
+            <!-- 成功提示框 -->
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <strong>成功!</strong> 操作成功提示！
+            </div>
+
+            <!-- 失败提示框 -->
+            <div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <strong>失败!</strong> 操作失败提示！
+            </div>
+
+            <!-- 自定义内容区域 -->
+            <div class="panel panel-default">
+                <div class="panel-heading">学生列表</div>
+                <table class="table table-striped table-hover table-responsive">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>姓名</th>
+                        <th>年龄</th>
+                        <th>性别</th>
+                        <th>添加时间</th>
+                        <th width="120">操作</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>18</td>
+                        <td>男</td>
+                        <td>2016-01-01</td>
+                        <td>
+                            <a href="">详情</a>
+                            <a href="">修改</a>
+                            <a href="">删除</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>18</td>
+                        <td>男</td>
+                        <td>2016-01-01</td>
+                        <td>
+                            <a href="">详情</a>
+                            <a href="">修改</a>
+                            <a href="">删除</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>18</td>
+                        <td>男</td>
+                        <td>2016-01-01</td>
+                        <td>
+                            <a href="">详情</a>
+                            <a href="">修改</a>
+                            <a href="">删除</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
     </div>
 </div>
 @endsection
